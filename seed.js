498e90622d0f0daad303b7a262d8eb83187a536d
HanifCahyo <hanifcahyoprasetyo95@gmail.com>
2023-07-30 17:19:27 +0700

    prefix seed

--- seed.js
+++ seed.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 
 // Connect to MongoDB via Mongoose
 seeder.connect(
-  "mongodb://127.0.0.1:27017/db_staycation",
+  "mongodb+srv://hanifcahyo:bwamern@cluster0.t2uraj0.mongodb.net/?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useCreateIndex: true,
